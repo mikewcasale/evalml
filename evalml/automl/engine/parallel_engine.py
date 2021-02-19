caa7637623d87294aeb678ec78f44f1efcf72196
Karsten Chu <karsten.chu@alteryx.com>
2021-02-19 17:17:45 -0500

    Removed lots of comments.

--- evalml/automl/engine/parallel_engine.py
+++ evalml/automl/engine/parallel_engine.py
@@ -40,10 +40,6 @@ class ParallelEngine(EngineBase):
             for pipeline in pipelines:
                 self._pre_evaluation_callback(pipeline)
 
-        # def train_and_score_pipeline(pipeline, automl, full_X_train, full_y_train):
-        #     self.train_and_score_pipeline(pipeline, automl, full_X_train, full_y_train)
-        #     return pipeline, func(pipeline, automl, full_X_train, full_y_train)
-
         pipeline_futures = self.client.map(self.train_and_score_pipeline, pipelines, automl=self.automl,
                                            full_X_train=self.X_train, full_y_train=self.y_train)
 
@@ -53,19 +49,3 @@ class ParallelEngine(EngineBase):
             new_pipeline_ids.append(self._post_evaluation_callback(pipeline, evaluation_result))
 
         return new_pipeline_ids
-        # if self.X_train is None or self.y_train is None:
-        #     raise ValueError("Dataset has not been loaded into the engine.")
-        # new_pipeline_ids = []
-        # index = 0
-        # while self._should_continue_callback() and index < len(pipelines):
-        #     pipeline = pipelines[index]
-        #     self._pre_evaluation_callback(pipeline)
-        #     evaluation_result = EngineBase.train_and_score_pipeline(pipeline, self.automl, self.X_train, self.y_train)
-        #     new_pipeline_ids.append(self._post_evaluation_callback(pipeline, evaluation_result))
-        #     index += 1
-        return new_pipeline_ids
-
-    # def train_and_score_pipeline(self, pipeline, automl, full_X_train, full_y_train):
-    #     import pdb; pdb.set_trace()
-    #     result = super().train_and_score_pipeline(pipeline, automl, full_X_train, full_y_train)
-    #     return result, pipeline
